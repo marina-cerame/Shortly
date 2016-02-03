fc0660c4d302a91e1835bfc108d2890dff6c4d0d
Dan Thareja <danthareja@gmail.com>
2016-02-03 15:34:00 -0800

    Add pomander and upgrade knex+bookshelf

--- app/config.js
+++ app/config.js
@@ -1,17 +1,11 @@
-var Bookshelf = require('bookshelf');
 var path = require('path');
-
-var db = Bookshelf.initialize({
+var knex = require('knex')({
   client: 'sqlite3',
   connection: {
-    host: '127.0.0.1',
-    user: 'your_database_user',
-    password: 'password',
-    database: 'shortlydb',
-    charset: 'utf8',
     filename: path.join(__dirname, '../db/shortly.sqlite')
   }
 });
+var db = require('bookshelf')(knex);
 
 db.knex.schema.hasTable('urls').then(function(exists) {
   if (!exists) {
