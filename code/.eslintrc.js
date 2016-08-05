d85487e5463760ac3877521c13299cc80e7f2e28
joshwyatt <joshuawyatt123@gmail.com>
2016-08-05 16:41:02 -0700

    Change links

--- code/.eslintrc.js
+++ code/.eslintrc.js
@@ -1,9 +1,9 @@
 /**
  * These rules enforce the Hack Reactor Style Guide:
- *   http://bookstrap.hackreactor.com/wiki/Style-Guide
+ *   https://github.com/reactorcore/docs.student-wiki/blob/master/Style-Guide.md
  *
  * Visit this repo for more information:
- *   https://github.com/hackreactor-labs/eslint-config-hackreactor
+ *   https://github.com/reactorcore/eslint-config-hackreactor
  */
 
 module.exports = {
