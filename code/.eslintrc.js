21570719ef25af859cf7aaae32e7933283176de8
joshwyatt <joshuawyatt123@gmail.com>
2016-09-28 10:36:48 -0700

    Remove link to style guide

--- code/.eslintrc.js
+++ code/.eslintrc.js
@@ -1,6 +1,5 @@
 /**
- * These rules enforce the Hack Reactor Style Guide:
- *   https://github.com/reactorcore/docs.student-wiki/blob/master/Style-Guide.md
+ * These rules enforce the Hack Reactor Style Guide
  *
  * Visit this repo for more information:
  *   https://github.com/reactorcore/eslint-config-hackreactor
